019d05b7c02d4fce868bed3a7afcf7f4e15fa341
lehos24 <kulise01@franklin.mail.edu>
2014-11-07 20:05:11 -0600

    Committing UserSettings.java class and user_settings.xml layout

---
+++ AndroidPodcastPlayer/src/edu/franklin/androidpodcastplayer/UserSettings.java
@@ -0,0 +1,12 @@
+
+package edu.franklin.androidpodcastplayer;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class UserSettings extends ActionBarActivity {
+
+	public UserSettings() {
+		
+	}
+
+}
