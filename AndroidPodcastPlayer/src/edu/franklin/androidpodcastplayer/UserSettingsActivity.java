b961f0db55b1096ec1d499b1d8c934cd149ad84a
lehos24 <kulise01@franklin.mail.edu>
2014-11-23 22:24:11 -0600

    Added the user settings functionality including database calls.

--- AndroidPodcastPlayer/src/edu/franklin/androidpodcastplayer/UserSettingsActivity.java
+++ AndroidPodcastPlayer/src/edu/franklin/androidpodcastplayer/UserSettingsActivity.java
@@ -1,17 +1,28 @@
 package edu.franklin.androidpodcastplayer;
 
+import java.util.ArrayList;
+
+import edu.franklin.androidpodcastplayer.data.ConfigData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class UserSettingsActivity extends ActionBarActivity {
-
+	
+	private ConfigData configData = new ConfigData(this);
+	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
+		configData.open();
+		configData.init();
 		setContentView(R.layout.user_settings);
+		this.updateCheckboxes();
 	}
 
 	@Override
@@ -24,9 +35,11 @@ public class UserSettingsActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		
 // Handle action bar item clicks here. The action bar will
 // automatically handle clicks on the Home/Up button, so long
 // as you specify a parent activity in AndroidManifest.xml.
+		
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
@@ -37,4 +50,64 @@ public class UserSettingsActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	/**
+	 * Reads the checkboxes and updates DB
+	 */
+	private void updateDB() {	
+		//data for wifi is index 0 and storage index 1		
+		ArrayList<Integer> val = new ArrayList<Integer>();
+		
+		CheckBox chkStore = (CheckBox) findViewById(R.id.chkSDcard);
+        CheckBox chkWifi = (CheckBox) findViewById(R.id.chkWifi);
+        
+        if(chkStore.isChecked()) {
+        	val.add(Integer.valueOf(1));
+        }
+        else {
+        	val.add(Integer.valueOf(0));
+        }
+		
+        if(chkWifi.isChecked()) {
+        	val.add(Integer.valueOf(1));
+        }
+        else {
+        	val.add(Integer.valueOf(0));
+        }
+        
+        configData.updateSettings(val);
+	}
+	
+	private void updateCheckboxes() {
+	       
+        CheckBox chkStore = (CheckBox) findViewById(R.id.chkSDcard);
+        CheckBox chkWifi = (CheckBox) findViewById(R.id.chkWifi);
+        
+     //get settings from db
+        ArrayList<Integer> arrData = configData.getSettings();
+        
+        if(arrData.get(0) == 1) {
+        	chkWifi.setChecked(true);
+        }
+        else {
+        	chkWifi.setChecked(false);        	
+        }
+        
+        if(arrData.get(1) == 1) {
+        	chkStore.setChecked(true);
+        }
+        else {
+        	chkStore.setChecked(false);        	
+        }
+		
+	}
+	
+	public void onClick_Update(View v) {
+        TextView textView = (TextView) findViewById(R.id.txtMsg); 
+        this.updateDB();
+        this.updateCheckboxes();
+        textView.setText("Settings Updated!");
+	
+	}
+	
 }
--- AndroidPodcastPlayer/src/edu/franklin/androidpodcastplayer/data/ConfigData.java
+++ AndroidPodcastPlayer/src/edu/franklin/androidpodcastplayer/data/ConfigData.java
@@ -1,14 +1,26 @@
 package edu.franklin.androidpodcastplayer.data;
 
+import java.util.ArrayList;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 
+/**
+ * ConfigData table will have only one record that
+ * is written to and read
+ * 
+ */
 public class ConfigData {
 
 	private DatabaseHelper dbHelper;
 	private SQLiteDatabase db;
-	private String[] allColumns = {  };
+	private String[] allColumns = {DatabaseHelper.CONFIG_COLUMN_CONFIGID, 
+			DatabaseHelper.CONFIG_COLUM_WIFIONLY, 
+			DatabaseHelper.CONFIG_COLUMN_EXTSTORAGE};
 
 	// Logcat tag
 	private static final String LOG = "ItemData";
@@ -29,6 +41,15 @@ public class ConfigData {
 	 */
 	public void open() throws SQLException {
 		db = dbHelper.getWritableDatabase();
+		
+	}
+	
+	public void init() {
+	//initial values for testing
+		ArrayList<Integer> val = new ArrayList<Integer>();
+		val.add(Integer.valueOf(1));
+		val.add(Integer.valueOf(0));
+		this.updateSettings(val);
 	}
 
 	/**
@@ -38,5 +59,39 @@ public class ConfigData {
 		dbHelper.close();
 	}
 	
+	public void updateSettings(ArrayList<Integer> settings) {
+	//find number of rows
+		SQLiteDatabase dbRead = dbHelper.getReadableDatabase();
+		String sql = "SELECT COUNT(*) FROM " + DatabaseHelper.TABLE_CONFIG;
+		SQLiteStatement statement = dbRead.compileStatement(sql);
+		Integer cnt = (int) statement.simpleQueryForLong();
+		
+	//clear the table if not empty
+		if(cnt != 0) {
+			db.delete(DatabaseHelper.TABLE_CONFIG, null, null);
+		}
+	//insert values
+			ContentValues newVal = new ContentValues();
+			newVal.put(DatabaseHelper.CONFIG_COLUM_WIFIONLY, settings.get(1));
+			newVal.put(DatabaseHelper.CONFIG_COLUMN_EXTSTORAGE, settings.get(0));
+			db.insert(DatabaseHelper.TABLE_CONFIG, null, newVal);
+	}
 	
+	public ArrayList<Integer> getSettings() {
+		
+		ArrayList<Integer> settings = new ArrayList<Integer>();
+		SQLiteDatabase readDB = dbHelper.getReadableDatabase();
+		Cursor cursor = readDB.query(DatabaseHelper.TABLE_CONFIG, allColumns,
+				null, null, null, null, null);
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()) {
+			if(!cursor.isNull(0)) {
+				settings.add(cursor.getInt(1));
+				settings.add(cursor.getInt(2));
+			}
+			cursor.moveToNext();
+		}
+		return settings;
+		
+	}	
 }
